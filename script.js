2756c10c17dc3a1b2788c01c433c40d9f4999e82
John Hauth <jkhauth@gmail.com>
2020-12-09 16:17:33 -0500

    style and functionality added to butttons

--- script.js
+++ script.js
@@ -8,40 +8,40 @@ var saveCities = JSON.parse(localStorage.getItem('Cities'))
 
 for (var i = 0; i < saveCities.length; i++){
   var saveCityButton = $('<button>')
-  $('#savedCities').append(saveCityButton);
+  $('#savedCities').prepend(saveCityButton);
   $(saveCityButton).text(saveCities[i])
   $(saveCityButton.attr('class', 'city'))
   $(saveCityButton.attr('id', saveCities[i]))
+  $(saveCityButton).addClass('align-center')
+  $(saveCityButton).addClass('btn btn-primary mt-2')
 }
-console.log()
 
 $('#citySearchBtn').click(function (e) { 
 e.preventDefault();
 var saveCityButton = $('<button>')
-var citySearch = $('#citySearch');
-var searchedCity = $('#citySearch').val();
+var searchedCity = $('#citySearch').val().trim();
+if ($('#citySearch').val() === ''){
+  alert("please enter a valid city")
+}else{
 $(this).attr('id', searchedCity)
+$(this).addClass('city');
 saveCities.push(searchedCity);
 $('#savedCities').append(saveCityButton)
 $(saveCityButton).text(searchedCity)
 $(saveCityButton).attr('class', 'city')
 $(saveCityButton).attr('id', searchedCity)
 localStorage.setItem('Cities', JSON.stringify(saveCities));
+}
 });
 
+//WHEN CITY SEARCH CLICKED
 $(document).on('click', '#citySearchBtn', searchCity)
-
-
 //WHEN SAVED CITY IS CLICKED
-$('.city').click(function (e) { 
-  e.preventDefault();
-});
-
 $(document).on('click', '.city', searchCity)
 
 //SEARCH A CITY
-function searchCity(){
-
+function searchCity(e){
+  e.preventDefault();
 var city = $(this).attr('id')
 var queryURL = 'http://api.positionstack.com/v1/forward?access_key=3db36236fe6861dd9972cda7b6b33cce&query='+city
 
@@ -59,6 +59,7 @@ var oneCall = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&l
 //MAIN TEXT INFORMATION
 var weathercity = response.data[0].label;
 var mainCitytext = $('#mainCitytext')
+var mainCityImg = $('#mainCityIcon')
 var mainCitydate = $('#mainCitydate')
 var mainCitytemp = $('#mainCitytemp')
 var mainCitywind = $('#mainCitywind')
@@ -102,18 +103,21 @@ $.ajax({
   var currentdateconv = date * 1000
   var currentdate = new Date (currentdateconv)
   $(mainCitydate).text(currentdate.toLocaleString())
-  $(mainCitytext).text('City Selected: ' + weathercity)
+  $(mainCitytext).text(weathercity)
+  $(mainCityImg).attr('src', 'http://openweathermap.org/img/wn/'+ response.current.weather[0].icon+'.png')
   $(mainCitytemp).text('Current Temp: ' + Math.round(((parseFloat(response.current.temp)-273.15)*1.8)+32)); // CURRENT TEMP
   $(mainCitywind).text('Current Wind Speed: ' + response.current.wind_speed); // CURRENT WINDSPEED
   $(mainCityUV).text('Current UV Index: ' + response.current.uvi); 
-
+  console.log(response)
   //5-DAY FORECAST DECELERATION
 
   // DAY ONE
   var dayoneDateresp = (response.daily[1].dt)
   var dayoneDateconv = dayoneDateresp * 1000
   var dayoneDate = new Date(dayoneDateconv)
+  var dayoneimg = ('#day-one-img')
   $(dayOnedate).text(dayoneDate.toLocaleString())
+  $(dayoneimg).attr('src','http://openweathermap.org/img/wn/'+ response.daily[1].weather[0].icon+'.png')
   $(dayOnetemp).text('TEMP: ' + Math.round(((parseFloat(response.daily[1].temp.day)-273.15)*1.8)+32))
   $(dayOnehumid).text('HUMIDITY: ' + response.daily[1].humidity)
 
@@ -121,38 +125,39 @@ $.ajax({
  var daytwoDateresp = (response.daily[2].dt)
  var daytwoDateconv = daytwoDateresp * 1000
  var daytwoDate = new Date(daytwoDateconv)
+ var daytwoimg = ('#day-two-img')
  $(dayTwodate).text(daytwoDate.toLocaleString())
+ $(daytwoimg).attr('src','http://openweathermap.org/img/wn/'+ response.daily[2].weather[0].icon+'.png')
  $(dayTwotemp).text('TEMP: ' + Math.round(((parseFloat(response.daily[2].temp.day)-273.15)*1.8)+32))
  $(dayTwohumid).text('HUMIDITY: ' + response.daily[2].humidity)
 
- // DAY ONE
+ // DAY THREE
  var daythreeDateresp = (response.daily[3].dt)
  var daythreeDateconv = daythreeDateresp * 1000
+ var daythreeimg = ('#day-three-img')
  var daythreeDate = new Date(daythreeDateconv)
  $(dayThreedate).text(daythreeDate.toLocaleString())
+ $(daythreeimg).attr('src','http://openweathermap.org/img/wn/'+ response.daily[3].weather[0].icon+'.png')
  $(dayThreetemp).text('TEMP: ' + Math.round(((parseFloat(response.daily[3].temp.day)-273.15)*1.8)+32))
  $(dayThreehumid).text('HUMIDITY: ' + response.daily[3].humidity)
 
- // DAY ONE
+ // DAY FOUR
  var dayfourDateresp = (response.daily[4].dt)
  var dayfourDateconv = dayfourDateresp * 1000
  var dayfourDate = new Date(dayfourDateconv)
+ var dayfourimg = ('#day-four-img')
  $(dayFourdate).text(dayfourDate.toLocaleString())
+ $(dayfourimg).attr('src','http://openweathermap.org/img/wn/'+ response.daily[4].weather[0].icon+'.png')
  $(dayFourtemp).text('TEMP: ' + Math.round(((parseFloat(response.daily[4].temp.day)-273.15)*1.8)+32))
  $(dayFourhumid).text('HUMIDITY: ' + response.daily[4].humidity)
 
- // DAY ONE
+ // DAY FIVE
  var dayfiveDateresp = (response.daily[5].dt)
  var dayfiveDateconv = dayfiveDateresp * 1000
+ var dayfiveimg = ('#day-five-img')
  var dayfiveDate = new Date(dayfiveDateconv)
  $(dayFivedate).text(dayfiveDate.toLocaleString())
+ $(dayfiveimg).attr('src','http://openweathermap.org/img/wn/'+ response.daily[5].weather[0].icon+'.png')
  $(dayFivetemp).text('TEMP: ' + Math.round(((parseFloat(response.daily[5].temp.day)-273.15)*1.8)+32))
  $(dayFivehumid).text('HUMIDITY: ' + response.daily[5].humidity)
-
-}  
-});
-
-},
-});
-}
-};
+}});},});}};
